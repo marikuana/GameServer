e1c846b45c2b5bc488ce6f32546652cb677819be
Marikuana <marians22092000@gmail.com>
2022-10-13 18:04:11 +0300

    Update Logger

---
+++ GameServer/Logger/ConsoleLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace GameServer
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"{DateTime.Now:[HH:mm:ss]} {message}");
+        }
+    }
+}
