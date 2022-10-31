8e96097d2e98bee266ec998217f377ad7b578ed3
Marikuana <marians22092000@gmail.com>
2022-10-31 21:26:22 +0200

    Update EntityBuilder add setTargetPos

--- GameServer/Entities/EntityBuilder.cs
+++ GameServer/Entities/EntityBuilder.cs
@@ -4,14 +4,23 @@ namespace GameServerCore
 {
     public class EntityBuilder : GameObjectBuilder<Entity>
     {
+        private IPosition? _targetPosition;
+
         public EntityBuilder(Entity gameObject) : base(gameObject)
         {
         }
 
+        public void SetTargerPosition(IPosition position)
+        {
+            _targetPosition = position;
+        }
+
         public override Entity Build()
         {
-            GameObject = base.Build();
-            
+            base.Build();
+
+            GameObject.SetTargetPosition(_targetPosition);
+
             return GameObject;
         }
     }
