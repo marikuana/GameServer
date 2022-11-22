db3c3bd70aee100bb996eaa3c61f4044ff35acd1
Marikuana <marians22092000@gmail.com>
2022-11-22 18:16:00 +0200

    Fix Startup | Add Db Project

---
+++ GameServerConsole/HostedService.cs
@@ -0,0 +1,35 @@
+﻿using GameServerCore;
+using Microsoft.Extensions.Hosting;
+
+namespace GameServerConsole
+{
+    public class HostedService : IHostedService
+    {
+        private PacketHandler _packetHandler;
+        private Server _server;
+        private SimulationService _simulationService;
+
+        public HostedService(PacketHandler packetHandler, Server server, SimulationService simulationService)
+        {
+            _packetHandler = packetHandler;
+            _server = server;
+            _simulationService = simulationService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _packetHandler.Init();
+            _server.Start();
+            _simulationService.StartAsync();
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _server.Stop();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- GameServerConsole/Program.cs
+++ GameServerConsole/Program.cs
@@ -2,18 +2,25 @@
 using System.Net;
 using GameServerCore;
 using Microsoft.Extensions.Logging;
+using GameServerData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 
 namespace GameServerConsole
 {
     public class Program
     {
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(ConfigureServices);          
+        }
+
         public static void Main(string[] args)
         {
-            var serviceCollection = new ServiceCollection();
-            IServiceCollection services = ConfigureServices(serviceCollection);
-            IServiceProvider serviceProvider = services.BuildServiceProvider();
-
-            Start(serviceProvider);
+            CreateHostBuilder(args)
+                .Build()
+                .Start();
 
             while (true)
             {
@@ -21,21 +28,30 @@ namespace GameServerConsole
             }
         }
 
-        public static IServiceCollection ConfigureServices(IServiceCollection services)
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
         {
+            services.AddHostedService<HostedService>();
+
             services.AddLogging(configure => {
                 configure.SetMinimumLevel(LogLevel.Trace);
                 configure.AddConsole();
             });
 
-            services.AddSingleton(new ConfigProvider().GetConfiguration());
+            Configuration configuration = new ConfigProvider().GetConfiguration();
+            services.AddSingleton(configuration);
 
-            services.AddTransient<EntityFactory>();
+            services.AddDbContext<Db>(options =>
+            {
+                options.UseSqlServer(configuration.ConnectionString);
+            });
+
+            services.AddSingleton<EntityFactory>();
+            services.AddSingleton<SpawnerFactory>();
 
             services.AddSingleton<EntityManager>();
             services.AddSingleton<GameObjectManager>();
             services.AddSingleton<IUpdate, GameObjectManager>(service => service.GetRequiredService<GameObjectManager>());
-            services.AddSingleton<IUpdate, FakeUpdate>();
+            //services.AddSingleton<IUpdate, FakeUpdate>();
             services.AddSingleton<SimulationService>();
 
             services.AddSingleton<Packets.PacketFactory>();
@@ -43,18 +59,6 @@ namespace GameServerConsole
             services.AddSingleton<SessionFactory>();
             services.AddSingleton<Server>();
 
-            return services;
-        }
-
-        public static void Start(IServiceProvider service)
-        {
-            var packetHandler = service.GetRequiredService<PacketHandler>();
-            var server = service.GetRequiredService<Server>();
-            var simulation = service.GetRequiredService<SimulationService>();
-
-            packetHandler.Init();
-            server.Start();
-            simulation.StartAsync();
         }
     }
 }
