ab1c7ecf3a9389430662868f71c006e330b70d65
Marikuana <marians22092000@gmail.com>
2023-01-30 12:46:03 +0200

    GameObject add OnChangePosition

--- GameServer/GameObject/GameObject.cs
+++ GameServer/GameObject/GameObject.cs
@@ -11,13 +11,27 @@ namespace GameServerCore
     public abstract class GameObject : IPosition
     {
         public Guid Id { get; set; }
-        public Vector3 Position { get; set; }
+        public Vector3 Position {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                if (position == value)
+                    return;
+                position = value;
+                OnChangePosition?.Invoke(this);
+            }
+        }
         public Vector3 Pos => Position;
         public Vector3 Rotation { get; set; }
 
         public event Action<GameObject>? OnDestroy;
+        public event Action<GameObject>? OnChangePosition;
 
         protected ILogger _logger;
+        private Vector3 position;
 
         public GameObject(ILogger logger)
         {
